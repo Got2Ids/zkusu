e02277329c7913bb01d7be199dd857d8f4459307
Got2Ids <90512448+Got2Ids@users.noreply.github.com>
2023-06-24 08:09:02 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 export UUID=f58b79fe-1292-4388-aac9-1126b93283c2
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=www.holehike.com
+export ProxySite=www.github.com/AYJCSGM/mikutap/archive/master.zip
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
